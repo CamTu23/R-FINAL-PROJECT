9b22f0c5e179ad61d46b9e02b1135c11973722fe
CamTu23 <tuntc19406@st.uel.edu.vn>
2022-07-23 11:27:49 +0700

    Update Clean_Convert_Data.R

--- LinearRegressionModel/Clean_Convert_Data.R
+++ LinearRegressionModel/Clean_Convert_Data.R
@@ -1,6 +1,29 @@
+#import library
 library(dplyr) #data manipulation
 library(ggplot2) #data visualization
 library(DT) #beautify the data table
 library(plotly) #make a pie chart
 library(Metrics) #find error value
+library(Hmisc)
+library(cowplot)
+library(WVPlots)
+
+#Load file
+data_insurance <- read.csv("./DATA/insurance.csv")
+
+#Understanding data
+describe(data_insurance)
+
+#EDA
+#Correlation age and charge
+
+#Correlation bmi and charge
+
+#Correlation charge and sex
+
+#Correlation charge and Children
+
+#Correlation charge and smoker
+
+#Correlation charge and Region
 
