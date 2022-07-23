cb029bf0593d8413bc36b0ef7f4411f589a98290
CamTu23 <tuntc19406@st.uel.edu.vn>
2022-07-23 11:12:36 +0700

    Load file, Understanding data

---
+++ LinearRegressionModel/Clean_Convert_Data.R
@@ -0,0 +1,6 @@
+library(dplyr) #data manipulation
+library(ggplot2) #data visualization
+library(DT) #beautify the data table
+library(plotly) #make a pie chart
+library(Metrics) #find error value
+
